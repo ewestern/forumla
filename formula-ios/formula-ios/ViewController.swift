f4b31297be159fbdc6126cee758f83d650a856ab
= <=>
2016-04-09 10:35:00 -0400

    initial ios commit

---
+++ formula-ios/formula-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  formula-ios
+//
+//  Created by Peter  France on 11/8/15.
+//  Copyright © 2015 Peter  France. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
